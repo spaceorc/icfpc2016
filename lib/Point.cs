02cddeb39675aec02451283622a26c4999f43210
Pavel Egorov <xoposhiy@mail.ru>
2016-08-05 19:43:59 +0500

    Point rotation

--- lib/Point.cs
+++ lib/Point.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics.Contracts;
 using System.Linq;
-using FluentAssertions;
-using NUnit.Framework;
 
 namespace lib
 {
@@ -78,73 +76,22 @@ namespace lib
 		}
 		public Rational ScalarProd(Vector p2)
 		{
-			return X*p2.X + Y*p2.Y;
+			return X * p2.X + Y * p2.Y;
 		}
 
-        public Rational VectorProdLength(Vector p2)
-        {
-            return X*p2.Y - p2.X * Y;
-        }
+		public Rational VectorProdLength(Vector p2)
+		{
+			return X * p2.Y - p2.X * Y;
+		}
 
 		[Pure]
 		public Vector Move(Rational shiftX, Rational shiftY)
 		{
 			return new Vector(X + shiftX, Y + shiftY);
 		}
-		
+
 		public double Length => Math.Sqrt(X * X + Y * Y);
 		public Rational Length2 => X * X + Y * Y;
 
-	}
-
-	public static class PointExtensions
-	{
-		public static Vector Reflect(this Vector p, Segment mirror)
-		{
-			var b = mirror.End - mirror.Start;
-			var a = p - mirror.Start;
-			var k = a.ScalarProd(b)*2/b.Length2;
-			return mirror.Start + b*k-a;
-		}
-
-		public static Vector[] ToPoints(this string points)
-		{
-			return points.Split(' ').Select(Vector.Parse).ToArray();
-		}
-	}
-
-
-
-	[TestFixture]
-	public class Point_Should
-	{
-		[TestCase("0,0 1,1", "0,1", "1,0")]
-		[TestCase("0,0 1,0", "0,1", "0,-1")]
-		[TestCase("10,10 11,10", "10,11", "10,9")]
-		public void BeMirrored(string segment, string point, string expectedPoint)
-		{
-			Segment s = segment;
-			Vector p = point;
-			var p2 = Vector.Parse(expectedPoint);
-			p.Reflect(s).Should().Be(p2);
-			p2.Reflect(s).Should().Be(p);
-		}
-
-		[TestCase("0,0", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Inside)]
-		[TestCase("0,0", "-1,1 1,1 1,-1 -1,-1", ExpectedResult = PointToPolygonPositionType.Inside)]
-		[TestCase("0,1/100000", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Inside)]
-		[TestCase("2,0", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Outside)]
-		[TestCase("-1,-1", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		[TestCase("-1,0", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		[TestCase("-1,0", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		[TestCase("-1,1", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		[TestCase("-1,-1", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		[TestCase("0,-1", "-1,-1 1,-1 1,1 -1,1", ExpectedResult = PointToPolygonPositionType.Boundary)]
-		public PointToPolygonPositionType BeInValidPositionToPolygon(string point, string polygonDef)
-		{
-            Vector p = point;
-			var polygon = new Polygon(polygonDef.Split(' ').Select(Vector.Parse).ToArray());
-			return p.GetPositionToPolygon(polygon);
-		}
 	}
 }
---
+++ lib/PointExtensions.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+
+namespace lib
+{
+	public static class PointExtensions
+	{
+		public static Vector Reflect(this Vector p, Segment mirror)
+		{
+			var b = mirror.End - mirror.Start;
+			var a = p - mirror.Start;
+			var k = a.ScalarProd(b)*2/b.Length2;
+			return mirror.Start + b*k-a;
+		}
+
+		public static Vector[] ToPoints(this string points)
+		{
+			return points.Split(' ').Select(Vector.Parse).ToArray();
+		}
+
+		public static Vector Rotate(this Vector p, Vector other, Rational x)
+		{
+			return (p - other).Rotate(x) + other;
+		}
+		public static Vector Rotate(this Vector p, Rational x)
+		{
+			Segment s1 = new Segment(new Vector(0, 0), new Vector(new Rational(1, 2), new Rational(1, 2)));
+			Segment s2 = x < 0
+				? new Segment(new Vector(0, 0), s1.End + new Vector(x, 0))
+				: new Segment(new Vector(0, 0), s1.End - new Vector(0, x));
+			return p.Reflect(s1).Reflect(s2);
+		}
+	}
+}
