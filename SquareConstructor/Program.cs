d74163112427ff9a3e22e557b97ec1748163ebd6
disturm <disturm@hotmail.com>
2016-08-06 15:21:11 +0500

    Отдельная форма для визуализации только полигонов и сементов

--- SquareConstructor/Program.cs
+++ SquareConstructor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using lib;
 
 namespace SquareConstructor
@@ -12,10 +13,16 @@ namespace SquareConstructor
 		static void Main(string[] args)
 		{
 			Segment s = new Segment(new Vector(-4, -10), new Vector(-7, -6));
-			Segment d = new Segment(new Vector(1, 2), new Vector(5, 5));
+			Segment d = new Segment(new Vector(1/2, 1/2), new Vector(1, 1));
 
-			var matrix = TransposeOperator.ConstructOperator(s, d);
-			Console.WriteLine();
-		}
+			//var matrix = TransposeOperator.ConstructOperator(s, d);
+			//Console.WriteLine();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var form = new PolygonsAndSegmentsForm();
+            form.SetData(null, null);
+            Application.Run(form);
+        }
 	}
 }
