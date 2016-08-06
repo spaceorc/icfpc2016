dcf2789a356e3fcd160ab8556df2454766e5b265
unknown <khr@khr2.kontur>
2016-08-06 15:26:08 +0500

    egmentsMatrixStarted

---
+++ SquareConstructor/SegmentsMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+
+namespace SquareConstructor
+{
+	class SegmentsMatrix
+	{
+		public SegmentMatrixNode[,] Matrix;
+
+		public SegmentsMatrix(int size)
+		{
+			Matrix = new SegmentMatrixNode[size, size];
+		}
+	}
+
+	class SegmentMatrixNode
+	{
+		public Dictionary<Segment, Polygon> Polygons = new Dictionary<Segment, Polygon>();
+
+		public void Add(Segment segment, Polygon polygon)
+		{
+			Polygons.Add(segment, polygon);
+		}
+
+		public void Remove(Segment segment)
+		{
+
+		}
+	}
+}
